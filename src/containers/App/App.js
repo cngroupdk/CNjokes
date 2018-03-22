8b47bbf92e191f021c3ac71aff3ffcaab1643941
Matúš Giertl <giertl@giertlmac2.local>
2018-03-22 21:36:26 +0100

    removed unnecessary imports

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import Category from '../../components/Category/Category';
 import Layout from '../Layout/Layout';
 
 
