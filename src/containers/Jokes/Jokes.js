68c03bce97a6bc82d65594cf75d82aab8481d7a7
Matúš Giertl <giertl@giertlmac2.domain.local>
2018-03-23 15:10:44 +0100

    usling lowercase css

--- src/containers/Jokes/Jokes.js
+++ src/containers/Jokes/Jokes.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Joke from "../../components/Joke/Joke";
 
-import "./Jokes.css";
+import "./jokes.css";
 
 import Jokes from  "../../mockdata/jokes.json";
 
