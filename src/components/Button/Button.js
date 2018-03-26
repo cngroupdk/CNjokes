1615e6b46e0065c0e6a16ee2800375d037758435
jakubarendac <arendac@cngroup.dk>
2018-03-26 10:31:31 +0200

    merged with origin

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
-import "./Button.css";
+import "./button.css";
 
 class Button extends Component {
   render() {
--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import './Joke.css';
+import './joke.css';
 
 export function Joke({ text }) {
   return (
