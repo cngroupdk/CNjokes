7cdebf81e2955a39bc6c69b1941e61266f8692c6
jakubarendac <arendac@cngroup.dk>
2018-03-26 10:39:55 +0200

    fixed conflict button.css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
-import "./button.css";
-
+import './button.css';
 class Button extends Component {
   render() {
     const { counterValue, incrementCounter, decrementCounter } = this.props;
