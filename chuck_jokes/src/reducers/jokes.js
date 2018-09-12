ceb2396adb29666f596a4cd56736c402775e08e0
Jakub Lajner <[jakub.lajner@gmail.com]>
2018-09-12 13:35:34 +0200

    basic actions added

--- chuck_jokes/src/reducers/jokes.js
+++ chuck_jokes/src/reducers/jokes.js
@@ -1,9 +1,11 @@
-import { FETCH_RANDOM_JOKE } from "../actions/actionTypes";
+import { FETCH_RANDOM_JOKE, SEARCH_JOKE } from "../actions/actionTypes";
 
 const jokes = (state = [], action) => {
   switch (action.type) {
     case FETCH_RANDOM_JOKE:
-      return action.payload;
+      return [action.payload];
+    case SEARCH_JOKE:
+      return [action.payload];
     default:
       return state;
   }
