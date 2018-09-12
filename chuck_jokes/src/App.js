6b859c7c02a55471d233cdf31d3deb6cbb145b9d
minndoo <milan.do81@gmail.com>
2018-09-12 10:55:09 +0200

    Milan (#12)
    * Basic css layout for the app
    
    * Basic CSS layout

--- chuck_jokes/src/App.js
+++ chuck_jokes/src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container box">
+        <div className="row box">
+          <div className="column box">blok1.1</div>
+          <div className="column box">blok1.2</div>
+          <div className="column box">blok1.3</div>
+        </div>
+        <div className="row box">
+          <div className="columnCategory box">blok2.1</div>
+          <div className="columnJoke box">blok2.2</div>
+        </div>
       </div>
     );
   }
