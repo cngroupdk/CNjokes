672fb55a3d4130b4610a0be5915bf11aa740c4e8
Jakub Lajner <[jakub.lajner@gmail.com]>
2018-09-26 10:52:40 +0200

    backup

--- cnjokes-gql-client/src/components/jokes/index.js
+++ cnjokes-gql-client/src/components/jokes/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
 import Joke from "./Joke";
@@ -12,16 +12,16 @@ const GET_RANDOM_JOKE = gql`
 `;
 
 const GET_JOKES_IN_CATEGORY = gql`
-  query {
-    jokesInCategory(category: "dev", limit: 3) {
+  query JokesInCategory($category: String!, $limit: Int!) {
+    jokesInCategory(category: $category, limit: $limit) {
       value
     }
   }
 `;
 
 const SEARCH_JOKES = gql`
-  query {
-    searchJokes(query: "car") {
+  query SearchJokes($query: String!) {
+    searchJokes(query: $query) {
       value
     }
   }
@@ -31,6 +31,14 @@ const GET_SEARCH_TERM = gql`
   query {
     searchTerm @client {
       value
+    }
+  }
+`;
+
+const GET_SELECTED_CATEGORY = gql`
+  query {
+    selectedCategory @client {
+      name
     }
   }
 `;
@@ -49,34 +57,88 @@ const renderJokes = jokes => {
   );
 };
 
-const Jokes = () => (
-  <Query query={GET_SEARCH_TERM}>
-    {({ data: { loading, searchTerm } }) => {
-      if (loading) return <p>Loading...</p>;
-      const query = searchTerm.value;
-      if (query && query.length > 0) {
-        return (
-          <Query query={SEARCH_JOKES} variables={{ query }}>
-            {({ loading, error, data }) => {
-              if (loading) return <div>Loading...</div>;
-              if (error) return <div>Error :(</div>;
-              return renderJokes(data.searchJokes);
-            }}
-          </Query>
-        );
-      } else {
-        return (
-          <Query query={GET_RANDOM_JOKE}>
-            {({ loading, error, data }) => {
-              if (loading) return <div>Loading...</div>;
-              if (error) return <div>Error :(</div>;
-              return renderJokes(data.randomJoke);
-            }}
-          </Query>
-        );
-      }
-    }}
-  </Query>
-);
+class Jokes extends Component {
+  state = { loadMore: false };
+
+  render() {
+    return (
+      // if search term is not empty string
+      // use SEARCH_JOKES query
+      <Query query={GET_SEARCH_TERM}>
+        {({ data: { loading, searchTerm } }) => {
+          if (loading) return <p>Loading...</p>;
+          const query = searchTerm.value;
+          if (query && query.length > 0) {
+            return (
+              // query returns max 25 jokes
+              <Query query={SEARCH_JOKES} variables={{ query }}>
+                {({ loading, error, data }) => {
+                  if (loading) return <div>Loading...</div>;
+                  if (error) return <div>Error :(</div>;
+                  return renderJokes(data.searchJokes);
+                }}
+              </Query>
+            );
+          } else {
+            // if search term is empty string
+            // use GET_SELECTED_CATEGORY query
+            return (
+              <Query query={GET_SELECTED_CATEGORY}>
+                {({ data: { loading, selectedCategory } }) => {
+                  if (loading) return <p>Loading...</p>;
+                  const selected = selectedCategory.name;
+                  // if loardMore is false
+                  // use GET_RANDOM_JOKE query
+                  if (!this.state.loadMore) {
+                    return (
+                      <Query query={GET_RANDOM_JOKE}>
+                        {({ loading, error, data }) => {
+                          if (loading) return <div>Loading...</div>;
+                          if (error) return <div>Error :(</div>;
+                          return (
+                            <div>
+                              {renderJokes(data.randomJoke)}
+                              <div className="load-more-container">
+                                <a
+                                  onClick={() =>
+                                    this.setState({
+                                      loadMore: true
+                                    })
+                                  }
+                                >
+                                  Load more...
+                                </a>
+                              </div>
+                            </div>
+                          );
+                        }}
+                      </Query>
+                    );
+                  } else {
+                    console.log(this.state);
+                    // if loadmore is true
+                    // use GET_JOKES_IN_CATEGORY query
+                    return (
+                      <Query
+                        query={GET_JOKES_IN_CATEGORY}
+                        variables={{ category: selected, limit: 100 }}
+                      >
+                        {({ loading, error, data }) => {
+                          if (loading) return <div>Loading...</div>;
+                          if (error) return <div>Error :(</div>;
+                          return renderJokes(data.jokesInCategory);
+                        }}
+                      </Query>
+                    );
+                  }
+                }}
+              </Query>
+            );
+          }
+        }}
+      </Query>
+    );
+  }
+}
 
 export default Jokes;
