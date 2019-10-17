1bd450af22ecdea79d93b19f61475331386683c7
Strojil Adam <strojil@strojilmac.domain.local>
2019-10-17 12:16:55 +0200

    feat(categorie-list): basic styling

--- jokes-app/src/components/CategoriesList.jsx
+++ jokes-app/src/components/CategoriesList.jsx
@@ -5,7 +5,7 @@ const API_URL = 'https://api.chucknorris.io/jokes/categories';
 class CategoriesList extends React.Component {
     constructor(props) {
         super(props)
-        
+
         this.state = {
             categories: []
         }
@@ -22,12 +22,14 @@ class CategoriesList extends React.Component {
     render() {
         return (
             <>
-            <p>This is the list of categories</p>
-                <ul>
-                    {this.state.categories.map((category) => {
-                        return <li>{category}</li>
-                    })}
-                </ul>
+                <div className="category-container">
+                    <ul>
+                        {this.state.categories.map((category) => {
+                            return <li>{category}</li>
+                        })}
+                        <li>- All -</li>
+                    </ul>
+                </div>
             </>
         );
     }
