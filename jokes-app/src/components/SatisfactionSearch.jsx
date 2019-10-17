d00a138fd322e40383ca8797a543be391ead29f8
Veronika Hniličková <hnilickova.vh@gmail.com>
2019-10-17 16:55:24 +0200

    feat(satisfaction-search): retrieving jokes by query

--- jokes-app/src/components/SatisfactionSearch.jsx
+++ jokes-app/src/components/SatisfactionSearch.jsx
@@ -1,30 +1,54 @@
 import React from 'react';
 
 const API_URL = 'https://api.chucknorris.io/jokes/search?query={query}';
+let finalUri = ''
 
 class SatisfactionSearch extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            query: ''
+            query: '',
+            searchedJokes: []
         }
     }
+
+    updateQueryStringParameter = () => {
+        const uri = API_URL;
+        const key = 'query';
+        const value = this.state.query;
+        const regex = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        finalUri = uri.replace(regex, '$1' + key + "=" + value);
+        console.log(finalUri);
+      }
+
+    componentDidMount = () => {
+        fetch(finalUri)
+          .then(response => response.json())
+          .then(dataFromApi => {
+              console.log(dataFromApi.result)
+            this.setState({ searchedJokes: dataFromApi.result });
+          });
+    };
 
     handleChange = (event) => {
         this.setState({query: event.target.value})
     }
 
     handleSearch = () => {
-        console.log(this.state.query);
+        this.updateQueryStringParameter();
+        this.componentDidMount();
     }
 
     render() {
+        const listItems = this.state.searchedJokes.map((joke) => <p key={joke.id}>{joke.value}</p>);
+
         return (
             <>
               <p>You can use this fulltext search to look for the jokes you're so eager to find.</p>
               <input type="search" value={this.state.query} onChange={this.handleChange}/>
               <button onClick={this.handleSearch}>Search!</button>
+              {listItems}
             </>
         );
     }
