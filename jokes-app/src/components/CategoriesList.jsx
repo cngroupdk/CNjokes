9e3cd4c39ef628f5094396a779a3e25072b28a51
Strojil Adam <strojil@strojilmac.domain.local>
2019-10-17 13:11:13 +0200

    feat(categories): tracking selected category, refactoring

--- jokes-app/src/components/CategoriesList.jsx
+++ jokes-app/src/components/CategoriesList.jsx
@@ -7,30 +7,47 @@ class CategoriesList extends React.Component {
         super(props)
 
         this.state = {
-            categories: []
+            categories: [],
+            selectedCategory: "- All -"
         }
     }
 
     componentDidMount = () => {
+        this.loadCategoriesFromAPI()
+        // Q: how about this.setState(getCategoriesFromAPI()) ?
+    };
+
+    loadCategoriesFromAPI = () => {
         fetch(API_URL)
             .then(response => response.json())
             .then(data => {
+                data.push("- All -") // let's add All category
                 this.setState({ categories: data });
             });
-    };
+    }
+
+    categoryClicked = (name) => {
+        this.setState({ selectedCategory: name })
+        console.log("Category " + name + " clicked!");
+    }
 
     render() {
         return (
-            <>
-                <div className="category-container">
-                    <ul>
-                        {this.state.categories.map((category) => {
-                            return <li>{category}</li>
-                        })}
-                        <li>- All -</li>
-                    </ul>
-                </div>
-            </>
+            <div className="category-container">
+                <ul>
+                    {this.state.categories.map((categoryName) => {
+                        return (
+                            <li
+                                key={categoryName}
+                                onClick={() => this.categoryClicked(categoryName)}
+                                className={categoryName === this.state.selectedCategory ? "selected" : ""}
+                            >
+                                {categoryName}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         );
     }
 }
