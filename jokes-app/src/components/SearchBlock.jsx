1d5f5a9874b7047069d5d06c34a870bb15151833
Veronika Hniličková <hnilickova.vh@gmail.com>
2019-10-22 12:36:15 +0200

    feat(modules): bootstrap added for reactstrap to work

--- jokes-app/src/components/SearchBlock.jsx
+++ jokes-app/src/components/SearchBlock.jsx
@@ -1,62 +1,72 @@
-import React from 'react';
-import { Button, Input } from 'reactstrap';
+import React from "react";
+import { Button, Input } from "reactstrap";
+import JokesList from "./JokesList";
 
-const API_URL = 'https://api.chucknorris.io/jokes/search?query=';
-let finalUri = ''
+const API_URL = "https://api.chucknorris.io/jokes/search?query=";
+let finalUri = "";
 
 class SearchBlock extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            query: '',
-            searchedJokes: [],
-            isQueryEmpty: true
-        }
+    this.state = {
+      query: "",
+      searchedJokes: [],
+      isQueryEmpty: true
+    };
+  }
+
+  updateQueryUrl = (API_URL, query) => {
+    finalUri = API_URL + query;
+    console.log(finalUri);
+  };
+
+  fetchData = () => {
+    fetch(finalUri)
+      .then(response => response.json())
+      .then(dataFromApi => {
+        console.log(dataFromApi.result);
+        this.setState({ searchedJokes: dataFromApi.result });
+      });
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.updateQueryUrl(API_URL, this.state.query);
+    if (this.state.query === "") {
+      this.setState({ isQueryEmpty: true, searchedJokes: [] });
+    } else {
+      this.setState({ isQueryEmpty: false });
+      this.fetchData();
     }
+  };
 
-    updateQueryUrl = (API_URL, query) => {
-        finalUri = API_URL + query
-        console.log(finalUri);
-      }
+  render() {
+    const listItems = this.state.searchedJokes.map(joke => joke.value);
 
-    fetchData = () => {
-        fetch(finalUri)
-                .then(response => response.json())
-                .then(dataFromApi => {
-                  console.log(dataFromApi.result)
-                  this.setState({ searchedJokes: dataFromApi.result });
-          });
-    }
-
-    handleChange = (event) => {
-        this.setState({query: event.target.value});
-    }
-
-    handleSearch = () => {
-        this.updateQueryUrl(API_URL, this.state.query);
-        if(this.state.query === ''){
-            this.setState({ isQueryEmpty: true, searchedJokes: []})
-        } else {
-            this.setState({ isQueryEmpty: false})
-            this.fetchData();
-        }
-        
-    }
-
-    render() {
-        const listItems = this.state.isQueryEmpty ? <p>Search for something!</p> :
-        this.state.searchedJokes.map((joke) => <p key={joke.id}>{joke.value}</p>);
-    
-        return (
-            <>
-              <p>You can use this fulltext search to look for the jokes you're so eager to find.</p>
-              <Input type="search" value={this.state.query} onChange={this.handleChange}/>
-              <Button onClick={this.handleSearch}>Search!</Button>
-              {listItems}
-            </>
-        );
-    }
+    return (
+      <>
+        <p>
+          You can use this fulltext search to look for the jokes you're so eager
+          to find.
+        </p>
+        <Input
+          type="search"
+          value={this.state.query}
+          onChange={this.handleChange}
+        />
+        <Button onClick={this.handleSearch}>Search!</Button>
+        {this.state.isQueryEmpty ? (
+          <p>Search for something!</p>
+        ) : (
+          <JokesList loaded={true} jokes={listItems} hasDuplicates={true} />
+        )}
+      </>
+    );
+  }
 }
 
 export default SearchBlock;
