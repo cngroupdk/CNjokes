3c79ee4bb49a1d8b582bb7ffe3f285988d7fbd1e
Veronika Hniličková <hnilickova.vh@gmail.com>
2019-10-24 16:49:54 +0200

    feat(categoryBlock): unsuccessful try to separe fetch

--- jokes-app/src/modules/api.js
+++ jokes-app/src/modules/api.js
@@ -16,12 +16,39 @@ export const api = {
       });
   },
   fetchSearchedJokes: (callback, query) => {
-    console.log(callback, query);
     fetch(`${API_URL}/search?query=${query}`)
       .then(response => response.json())
       .then(data => {
         callback({ ...data });
       });
+  },
+  // the function below does not work as required...
+  fetchAmountOfJokes: (callback, category, numberOfJokes) => {
+    let jokes = [];
+    let jokesLoaded = 0;
+    let apiUrl = "";
+
+    if (category !== "all") {
+      apiUrl = `${API_URL}/random?category=${category}`;
+    } else {
+      apiUrl = `${API_URL}/random`;
+    }
+
+    for (let i = 0; i < numberOfJokes; i++) {
+      fetch(`${apiUrl}`)
+        .then(response => response.json())
+        .then(data => {
+          jokes.push(data.value);
+          jokesLoaded++;
+          if (jokesLoaded === numberOfJokes) {
+            const uniqueJokes = [...new Set(jokes)];
+            const hasDuplicateJokes =
+              uniqueJokes.length === numberOfJokes ? false : true;
+            console.log(uniqueJokes);
+            callback({ ...uniqueJokes, hasDuplicateJokes });
+          }
+        });
+    }
   },
   fetchSthElse: () => {} // add other endpoints
 };
