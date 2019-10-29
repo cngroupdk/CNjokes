72447fbaac0e8ad859d450bb04d37c6ef748c416
Manoel Cunha <cunha@cngroup.dk>
2019-10-29 15:36:24 +0100

    fixing cors issue

--- vue-jokes/src/modules/api.js
+++ vue-jokes/src/modules/api.js
@@ -1,8 +1,10 @@
-const API_URL = "http://localhost:3000/jokes/";
+const API_URL = "http://api.chucknorris.io/jokes/";
 
 export const api = {
   fetchCategories: callback => {
-    fetch(`${API_URL}categories`)
+    fetch(`${API_URL}categories`, {
+        mode: 'no-cors',
+    })
       .then(response => response.json())
       .then(data => callback(data));
   },
@@ -15,7 +17,9 @@ export const api = {
         : formInputs.searchInputText;
     fetch(
       `${API_URL}${formInputs.numberOfJokes}/${reqCategory}/${reqSearchedText}`
-    )
+    , {
+        mode: 'no-cors',
+    })
       .then(response => response.json())
       .then(data => callback([...data]));
   }
