1b61c5613fa1eb6e8a2ad2d0b44be01477362853
Veronika Hniličková <hnilickova.vh@gmail.com>
2019-10-29 09:12:36 +0100

    feat(categoriesList): re-creating categoriesList component into a dumb component

--- jokes-app/src/components/CategoriesList.jsx
+++ jokes-app/src/components/CategoriesList.jsx
@@ -1,47 +1,23 @@
 import React from "react";
-import { api } from "../modules/api";
 
-class CategoriesList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      categories: []
-    };
-  }
-
-  componentDidMount = () => {
-    api.fetchCategories(this.setCategoriesToState);
-  };
-
-  setCategoriesToState = data => {
-    this.setState({ categories: data });
-  };
-
-  categoryClicked = name => {
-    this.props.categorySetter(name);
-  };
-
-  render() {
-    return (
-      <div className="category-container">
-        <ul>
-          {this.state.categories.map(categoryName => {
-            const { selectedCategory } = this.props;
-            return (
-              <li
-                key={categoryName}
-                onClick={() => this.categoryClicked(categoryName)}
-                className={categoryName === selectedCategory ? "selected" : ""}
-              >
-                {categoryName}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+function CategoriesList({ categories, categorySetter, selectedCategory }) {
+  return (
+    <div className="category-container">
+      <ul>
+        {categories.map(categoryName => {
+          return (
+            <li
+              key={categoryName}
+              onClick={() => categorySetter(categoryName)}
+              className={categoryName === selectedCategory ? "selected" : ""}
+            >
+              {categoryName}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default CategoriesList;
